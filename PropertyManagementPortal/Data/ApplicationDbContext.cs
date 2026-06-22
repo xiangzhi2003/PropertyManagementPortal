4973e47717492671f34fbda39447bca8c4ad2b46
CHIANG XIANG ZHI <xiangzhichiang2003@gmail.com>
2026-06-22 23:44:01 +0800

    Initial commit

---
+++ PropertyManagementPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PropertyManagementPortal.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
