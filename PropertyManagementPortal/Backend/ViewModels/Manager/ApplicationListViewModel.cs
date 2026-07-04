d8d5bd0dc51546bb6a27ca666b88db7f4cfa2cdd
stanleywijayaa <stanleytan2018@gmail.com>
2026-07-04 18:56:51 +0800

    added model for application lists (tenant name, email, property, unit,etc)

---
+++ PropertyManagementPortal/Backend/ViewModels/Manager/ApplicationListViewModel.cs
@@ -0,0 +1,23 @@
+namespace PropertyManagementPortal.ViewModels.Manager
+{
+    public class ApplicationListViewModel
+    {
+        public List<ApplicationRowViewModel> Applications { get; set; } = new();
+        public string? StatusFilter { get; set; }
+        public int PendingCount { get; set; }
+    }
+ 
+    public class ApplicationRowViewModel
+    {
+        public int TenancyId { get; set; }
+        public string TenantName { get; set; } = "";
+        public string TenantEmail { get; set; } = "";
+        public string PropertyName { get; set; } = "";
+        public string UnitNumber { get; set; } = "";
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Status { get; set; } = "";
+        public DateTime CreatedAt { get; set; }
+        public string? Notes { get; set; }
+    }
+}
