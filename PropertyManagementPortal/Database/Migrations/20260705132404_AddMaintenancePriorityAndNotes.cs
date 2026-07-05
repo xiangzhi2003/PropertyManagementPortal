a1eb2f563306e5ba91811fd9e9740110d35a4e04
stanleywijayaa <stanleytan2018@gmail.com>
2026-07-05 22:16:07 +0800

    updated migration files to contain new fields on MaintenanceRequest

---
+++ PropertyManagementPortal/Database/Migrations/20260705132404_AddMaintenancePriorityAndNotes.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PropertyManagementPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMaintenancePriorityAndNotes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AssignmentNotes",
+                table: "MaintenanceRequests",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Priority",
+                table: "MaintenanceRequests",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssignmentNotes",
+                table: "MaintenanceRequests");
+
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "MaintenanceRequests");
+        }
+    }
+}
