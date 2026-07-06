27f111f0bf92bbdaa51bd76abbf06ef3d6888e24
Jinxsuxk <marvinjaylorenz@gmail.com>
2026-07-07 00:41:59 +0800

    fix BrowseUnit UI

---
+++ PropertyManagementPortal/Backend/ViewModels/Tenant/ApplyViewModel.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PropertyManagementPortal.ViewModels.Tenant
+{
+    public class ApplyUnitViewModel
+    {
+        public int UnitId { get; set; }
+
+        public string PropertyName { get; set; } = "";
+
+        public string UnitNumber { get; set; } = "";
+
+        public decimal RentAmount { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+    }
+}
